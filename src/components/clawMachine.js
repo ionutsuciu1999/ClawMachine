d6b1a11b14942a4f3ce32bb60abe1ebd46faae09
Ionut Suciu <suciuionut888@gmail.com>
2024-02-20 17:18:31 +0100

    start of project layout

--- src/components/clawMachine.js
+++ src/components/clawMachine.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-const clawMachine () => {
+const ClawMachine = (props) => {
 
+    return(
+        <div id="clawMachineContainer">
+            <div id="clawMachine">
+                <img id="clawFront" src="../front.png"/>
+                <img id="clawClaw" src="../clawclosed.png"/>
+                <img id="clawBack" src="../back.png"/>
+            </div>
+        </div>
+    )
 
-    
 }
 
-export default clawMachine;
+export default ClawMachine;
