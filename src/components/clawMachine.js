be979d492d78f3bb176d3c3fbbf12c621157038c
Ionut Suciu <suciuionut888@gmail.com>
2024-02-21 17:23:13 +0100

    added functionality and responsive graphics

--- src/components/clawMachine.js
+++ src/components/clawMachine.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ClawMachine = (props) => {
+    let [money,setMoney] = useState(0);
 
     return(
         <div id="clawMachineContainer">
             <div id="clawMachine">
+                <img id="clawLeftPressed" src="../leftPressed.png"/>
+                <img id="clawLeft" src="../left.png"/>
+                <img id="clawRightPressed" src="../rightPressed.png"/>
+                <img id="clawRight" src="../right.png"/>
+                <img id="clawGoPressed" src="../goPressed.png"/>
+                <img id="clawGo" src="../go.png"/>
+                <img id="clawMoneyPressed" src="../moneyPressed.png"/>
+                <img id="clawMoney" src="../money.png"/>
+                <span id="money">{money}</span>
+
                 <img id="clawFront" src="../front.png"/>
+                
                 <img id="clawClaw" src="../clawclosed.png"/>
                 <img id="clawBack" src="../back.png"/>
             </div>
