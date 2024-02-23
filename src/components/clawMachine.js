18038fe359fa8fa2dafcda0feadaeaeb08ce6903
Ionut Suciu <suciuionut888@gmail.com>
2024-02-23 17:27:39 +0100

    try to add async func. for go button

--- src/components/clawMachine.js
+++ src/components/clawMachine.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 
 const ClawMachine = (props) => {
     let [money,setMoney] = useState(0);
-    let [left,setLeft] = useState(10);
+    let [left,setLeft] = useState(12);
     let [top,setTop] = useState(10);
     let [open,setOpen] = useState(0);
     let moving = 0;
@@ -10,6 +10,8 @@ const ClawMachine = (props) => {
     //get updated left val state inside setTimeout
     const leftRef = useRef(left);
     leftRef.current = left;
+    const topRef = useRef(top);
+    topRef.current = top;
 
     const stopMove = () =>{
         console.log("Stop");
@@ -17,39 +19,47 @@ const ClawMachine = (props) => {
     }
 
     const moveLoop = (direction,amount) =>{
-        setTimeout(() => {
-            console.log(leftRef.current);
-            console.log(direction);
-            if(direction=="right" && leftRef.current < 70){
-                setLeft((left)=>left+amount);
-            }else if(direction=="left" && leftRef.current > 11){
-                setLeft((left)=>left-amount);
-            }else if(direction=="down"){
-                setTop((top)=>top+amount);
-            }
+
+        //make a for loop & put set timeout in a promise
+
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                if(direction=="horizontal" && ((leftRef.current+amount) < 70) && ((leftRef.current+amount) > 10)){
+                    setLeft((left)=>left+amount);
+                }else if(direction=="vertical"){
+                    setTop((top)=>top+amount);
+                }
+                resolve("ok");
+            }, 10);
+        });
+    }
+
+    const moveee = async (direction,amount,distance) =>{
+        for(var i = 0; i<distance; i++){
             
             if(moving == 1){
-                moveLoop(direction,amount);
+                const result = await moveLoop(direction,amount).then((value)=>{console.log("INF")});
+            } else {
+                console.log("fin");
+                //not getting returned because its recursive
             }
-        }, 10);
+        }
     }
 
-    const move = (e) =>{
+    function move(e){
         console.log(e.target.id);
         moving = 1;
-        if(e.target.id=="clawRight"){moveLoop("right",0.5)}
-        if(e.target.id=="clawLeft"){moveLoop("left",0.5)}
-        if(e.target.id=="clawGo"){getPrize()}
+        if(e.target.id=="clawRight"){moveee("horizontal",0.5,999)}
+        if(e.target.id=="clawLeft"){moveee("horizontal",-0.5,999)}
+        if(e.target.id=="clawGo"){
+            console.log("gerpize");
+            setOpen(1);
+            moving = 1;
+            console.log('calling');
+            moveee("horizontal",-0.5,22);
+        }
     }
-
-    const getPrize = () =>{
-        console.log("gerpize");
-        setOpen(1);
-        moveLoop("down",20);
-    }
-    const moveDown = () =>{
-
-    }
+   
 
     useEffect(() => {
         document.getElementById("clawRight").addEventListener("mousedown",move);
@@ -58,14 +68,14 @@ const ClawMachine = (props) => {
         document.getElementById("clawRight").addEventListener("mouseup",stopMove);
         document.getElementById("clawLeft").addEventListener("dragend",stopMove);
         document.getElementById("clawLeft").addEventListener("mouseup",stopMove);
-        document.getElementById("clawGo").addEventListener("click",getPrize);
+        document.getElementById("clawGo").addEventListener("click",move);
 
         return () => {
             document.getElementById("clawRight").removeEventListener("mousedown",move);
             document.getElementById("clawLeft").removeEventListener("mousedown",move);
             document.removeEventListener("dragend",stopMove);
             document.removeEventListener("mouseup",stopMove);
-            document.getElementById("clawGo").removeEventListener("click",getPrize);
+            document.getElementById("clawGo").removeEventListener("click",move);
         }
     },[]);
 
